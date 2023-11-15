8eec24b23833b03f9f2460e76798030d81fafe91
positivelyjon <positivelyjon@gmail.com>
2023-11-16 01:31:52 +0800

    Implement color change when mouse enters box

--- script.js
+++ script.js
@@ -1,12 +1,20 @@
 const grid = document.querySelector('.grid');
+const box = document.querySelectorAll('.box');
+
 for (let i = 0; i < 16; i++) {
     const row = document.createElement('div');
     row.setAttribute('class', 'row');
     for (let j = 0; j < 16; j++) {
         const box = document.createElement('div');
         box.setAttribute('class', 'box');
+        function changeColor() {
+            box.style.background = 'cyan';
+        }
+        box.addEventListener('mouseenter', changeColor);
         row.appendChild(box);
     }
     grid.appendChild(row);
     
 }
+
+
