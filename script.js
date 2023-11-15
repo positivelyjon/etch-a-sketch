99ac01204ed4f27ed0291d019a005bb3229af153
positivelyjon <positivelyjon@gmail.com>
2023-11-16 02:14:29 +0800

    Allow user to change squares in 960px x 960px grid

--- script.js
+++ script.js
@@ -1,20 +1,40 @@
-const grid = document.querySelector('.grid');
+const body = document.querySelector('body');
 const box = document.querySelectorAll('.box');
+const button = document.querySelector('button');
 
-for (let i = 0; i < 16; i++) {
-    const row = document.createElement('div');
-    row.setAttribute('class', 'row');
-    for (let j = 0; j < 16; j++) {
-        const box = document.createElement('div');
-        box.setAttribute('class', 'box');
-        function changeColor() {
-            box.style.background = 'cyan';
+function gridMaker(n) {
+    body.removeChild(document.querySelector('.grid'));
+    const new_grid = document.createElement('div');
+    new_grid.setAttribute('class', 'grid');
+    for (let i = 0; i < n; i++) {
+        const row = document.createElement('div');
+        row.setAttribute('class', 'row');
+        for (let j = 0; j < n; j++) {
+            const box = document.createElement('div');
+            box.setAttribute('class', 'box');
+            function changeColor() {
+                box.style.background = 'cyan';
+            }
+            box.addEventListener('mouseenter', changeColor);
+            row.appendChild(box);
         }
-        box.addEventListener('mouseenter', changeColor);
-        row.appendChild(box);
+        new_grid.appendChild(row);    
     }
-    grid.appendChild(row);
-    
+    body.appendChild(new_grid);
 }
+
+gridMaker(16);
+
+function changeSize() {
+    const side = prompt("How many boxes do you want on each side of the grid?");
+    const pixelSize = Math.floor(960 / side);
+    gridMaker(side);
+    box.forEach(box => {
+        box.width = pixelSize;
+        box.style.height = pixelSize;
+    })
+}
+
+button.addEventListener('click', changeSize);
 
 
