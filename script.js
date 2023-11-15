12cf9caa390d598c43bc06d3bd9f3b77a143535d
positivelyjon <positivelyjon@gmail.com>
2023-11-16 01:10:26 +0800

    Adjust grid to be 16x16 instead of 4x4

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 const grid = document.querySelector('.grid');
-for (let i = 0; i < 4; i++) {
+for (let i = 0; i < 16; i++) {
     const row = document.createElement('div');
     row.setAttribute('class', 'row');
-    for (let j = 0; j < 4; j++) {
+    for (let j = 0; j < 16; j++) {
         const box = document.createElement('div');
         box.setAttribute('class', 'box');
         row.appendChild(box);
